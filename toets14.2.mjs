7abb893d76aeeaa13edaccde2ff015aaf9fccf7a
y-messan <151385876+y-messan@users.noreply.github.com>
2024-09-30 13:19:38 +0200

    Create toets14.2.mjs

---
+++ toets14.2.mjs
@@ -0,0 +1,13 @@
+
+let i = 1
+while ( i<=3){
+      let j=1;
+      let rij= ''
+       while ( j<=4 ){
+        let som = j*i;
+       rij +=som +' '
+       j++; 
+    }  
+    console.log (rij); 
+     i++; 
+}
