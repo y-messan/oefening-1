475d19647b846c452e534d36912116f82ec11335
y-messan <151385876+y-messan@users.noreply.github.com>
2024-10-01 11:10:14 +0200

    toetsen

---
+++ toets16.mjs
@@ -0,0 +1,5 @@
+function optellen (getal1 , getal2){
+    let som = getal1 + getal2;
+    console.log ('De som van '+getal1 +' en '+ getal2 + ' is ' + som);
+}
+optellen ( 5, 6 )
---
+++ toets18.mjs
@@ -0,0 +1,5 @@
+function maalDrie(num){
+   return (num*3);
+}
+let resultaat = maalDrie(3)
+console.log (resultaat)
